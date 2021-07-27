b586c167c26e89b8f64c3eb023250232518a640f
dinggididudara <dinggididududu@gmail.com>
2021-07-26 22:48:41 -0400

    .

--- Lab6/src/Library/LibraryManagement.java
+++ Lab6/src/Library/LibraryManagement.java
@@ -5,12 +5,32 @@ import java.util.Scanner;
 public class LibraryManagement implements Policies{
 	public static void main(String[] args) {
 		String name; //library name
+		int option;
 	
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.printf("What is the library's name? : ");
+		System.out.print("What is the library's name? : ");
 		name = sc.next();
-	}
+		System.out.println("1. Read from keyboard");
+		System.out.println("2. Read from file");
+		System.out.println("3. Check fines");
+		System.out.print("Choose your option: ");
+		option = sc.nextInt();
+		while(true) {	
+			switch(option) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				default:
+					continue;
+			} //switch-case end
+		} //while end
+	} //main end
 	
 	
 	@Override
@@ -24,4 +44,4 @@ public class LibraryManagement implements Policies{
 		
 		
 	}
-}
+} //LibraryManagement class end
